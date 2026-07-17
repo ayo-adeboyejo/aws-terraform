0e8d3e304f7618f6acf2e364733c30d1262b42b0
ayo-adeboyejo <ae.adeboyejo@gmail.com>
2026-07-17 20:14:23 +0100

    modify output attributes

--- aws-s3-cloudfront-webhosting-terraform/output.tf
+++ aws-s3-cloudfront-webhosting-terraform/output.tf
@@ -5,3 +5,12 @@ output "aws_route53_name_servers" {
 output "cloudfront_distribution_domain_name" {
   value = aws_cloudfront_distribution.main_s3_distribution.domain_name
 }
+
+output "s3_bucket_name" {
+  value = aws_s3_bucket.main_bucket.id
+}
+
+
+output "route53_zone_id" {
+  value = aws_route53_zone.main_route.id
+}
